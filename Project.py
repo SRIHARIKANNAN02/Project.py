e176bfa482839ea563e6163c9461247783f62381
SRIHARIKANNAN02 <111687313+SRIHARIKANNAN02@users.noreply.github.com>
2022-09-04 16:34:41 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,14 @@
+from re import I
+
+
+num = int(input("Enter a number ( greater than 1)"))
+f = 0
+i = 2
+
+ while i <= num / 2:
+  if num % i == 0:
+     f=1
+     print("The entered number is not a PRIME number")
+
+    i=i+1     
+    
